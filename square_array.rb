011d0121e341ae13b3f669e3136d968fb14c4521
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-21 23:05:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
-  #squared_array = []
-  #array.each do |item|
-  #  squared_array.push(item ** 2)
-  #end
-  #squared_array
-  array.collect { |x| x ** 2 }
+  squared_array = []
+  array.each do |item|
+    squared_array.push(item ** 2)
+  end
+  squared_array
 end
